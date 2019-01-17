02ae84867d73ec011f3e63c8de55167f6fbefcb9
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2019-01-17 22:58:24 +0100

    Add a mean and stddev, normalization offet compute and ideas

---
+++ compute-mean-std.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+# Note, using the Welford method with running mean and variance is slow.
+# But since it has to be done only once it might be worth it
+
+# Image sizes go from 5959x695 to 77x30. Numpy needs the same resolution for all.
+RESOLUTION = 224 # completely arbitrary :P.
+
+if __name__ == "__main__":
+    data = []
+    df_train = pd.read_csv('./input/train.csv')
+
+    for file in tqdm(df_train['Image'], miniters=256):
+        img = cv2.imread(f'./input/train/{file}')
+        data.append(cv2.resize(img,(RESOLUTION,RESOLUTION)))
+
+    data = np.array(data, np.float32) / 255 # Must use float32 at least otherwise we get over float16 limits
+    print("Shape: ", data.shape)
+
+    means = []
+    stdevs = []
+    for i in range(3):
+        pixels = data[:,:,:,i].ravel()
+        means.append(np.mean(pixels))
+        stdevs.append(np.std(pixels))
+
+    print("means: {}".format(means))
+    print("stdevs: {}".format(stdevs))
+    print('transforms.Normalize(mean = {}, std = {})'.format(means, stdevs))
+
+# Shape:  (25361, 224, 224, 3)
+# means: [0.6073162, 0.5655911, 0.528621]
+# stdevs: [0.26327327, 0.2652084, 0.27765632]
+# transforms.Normalize(mean = [0.6073162, 0.5655911, 0.528621], std = [0.26327327, 0.2652084, 0.27765632])
---
+++ ideas.md
@@ -0,0 +1,14 @@
+# Some ideas to improve modelling
+
+- Due to the large difference in image size, rescaling may not be the best preprocessing.
+  It might be useful to use black or white as padding (and teaching the NN to ignore it)
+  Rescaling might produce artifacts, reduce a characteristic
+  or distract the NN by enlarging something irrelevant like water.
+
+
+
+# NN Arch
+
+- Siamese Networks
+- Generative Adversarial Network
+- Re-Identification networks - https://github.com/KaiyangZhou/deep-person-reid and https://github.com/KaiyangZhou/deep-person-reid/blob/master/AWESOME_REID.md
